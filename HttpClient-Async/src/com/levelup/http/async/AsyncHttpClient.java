b7c8388bd1a01799c373dba5e26c377200d31212
Steve Lhomme <slhomme@levelupstudio.com>
2014-04-07 16:19:42 +0200

    better thread safety

--- HttpClient-Async/src/com/levelup/http/async/AsyncHttpClient.java
+++ HttpClient-Async/src/com/levelup/http/async/AsyncHttpClient.java
@@ -3,8 +3,12 @@ package com.levelup.http.async;
 import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -13,7 +17,6 @@ import android.os.Handler;
 import android.os.Looper;
 
 import com.levelup.http.HttpClient;
-import com.levelup.http.HttpException;
 import com.levelup.http.HttpRequest;
 import com.levelup.http.HttpRequestGet;
 
@@ -22,14 +25,15 @@ import com.levelup.http.HttpRequestGet;
  */
 public class AsyncHttpClient {
 
+	private static final int THREAD_POOL_SIZE = 3*Runtime.getRuntime().availableProcessors();
+	private static final BlockingQueue<Runnable> sPoolWorkQueue = new LinkedBlockingQueue<Runnable>();
+
+	public static AsyncHttpClient instance = new AsyncHttpClient();
+
 	private final HashMap<String, Future<?>> taggedJobs = new HashMap<String, Future<?>>();
 	private final Handler uiHandler;
 
-	private final static int THREAD_POOL_SIZE = 3*Runtime.getRuntime().availableProcessors();
-	private static final BlockingQueue<Runnable> sPoolWorkQueue = new LinkedBlockingQueue<Runnable>();
 	private ExecutorService executor = new ThreadPoolExecutor(0, THREAD_POOL_SIZE, 60, TimeUnit.SECONDS, sPoolWorkQueue);
-
-	public static AsyncHttpClient instance = new AsyncHttpClient();
 
 	private AsyncHttpClient() {
 		this.uiHandler = new Handler(Looper.getMainLooper());
@@ -71,50 +75,55 @@ public class AsyncHttpClient {
 	 * @param httpListener listener for data received or errors
 	 */
 	public void getString(final HttpRequest req, final String tag, final AsyncHttpListener<String> httpListener) {
-		synchronized (taggedJobs) {
-			if (null!=tag) {
-				Future<?> oldJob = taggedJobs.get(tag);
-				if (null!=oldJob) {
-					oldJob.cancel(true);
-				}
+		Callable<String> netReq = new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				return HttpClient.getStringResponse(req);
 			}
+		};
+		
+		FutureTask<String> task = new FutureTask<String>(netReq) {
+			@Override
+			protected void done() {
+				super.done();
 
-			final Future<?> newJob = executor.submit(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						final String result = HttpClient.getStringResponse(req);
-						if (null!=httpListener && !Thread.interrupted())
-							uiHandler.post(new Runnable() {
-								@Override
-								public void run() {
-									//if (!newJob.isCancelled())
-									httpListener.onHttpSuccess(result);
+				if (null!=httpListener)
+					uiHandler.post(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								String result = get();
+								httpListener.onHttpSuccess(result);
+							} catch (CancellationException e) {
+							} catch (InterruptedException e) {
+							} catch (ExecutionException e) {
+								Throwable t = e.getCause();
+								if (t.getCause() instanceof SocketTimeoutException)
+									httpListener.onHttpError(t.getMessage(), AsyncHttpListener.ErrorType.Timeout);
+								else
+									httpListener.onHttpError(t.getMessage(), AsyncHttpListener.ErrorType.Generic);
+							} finally {
+								if (null!=tag) {
+									synchronized (taggedJobs) {
+										taggedJobs.remove(tag);
+									}
 								}
-							});
-					} catch (final HttpException e) {
-						if (null!=httpListener) 
-							uiHandler.post(new Runnable() {
-								@Override
-								public void run() {
-									if (e.getCause() instanceof SocketTimeoutException)
-										httpListener.onHttpError(e.getMessage(), AsyncHttpListener.ErrorType.Timeout);
-									else
-										httpListener.onHttpError(e.getMessage(), AsyncHttpListener.ErrorType.Generic);
-								}
-							});
-					} finally {
-						if (null!=tag) {
-							synchronized (taggedJobs) {
-								taggedJobs.remove(tag);
 							}
 						}
-					}
-				}
-			});
-			if (null!=tag) {
-				taggedJobs.put(tag, newJob);
+					});
 			}
+		};
+
+		synchronized (taggedJobs) {
+			if (null!=tag) {
+				Future<?> oldTask = taggedJobs.get(tag);
+				if (null != oldTask) {
+					oldTask.cancel(true);
+				}
+				taggedJobs.put(tag, task);
+			}
+
+			executor.execute(task);
 		}
 	}
 }
