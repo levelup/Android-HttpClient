a67768c424cb1f365213e21949f101fc64fb9b35
Steve Lhomme <robux4@gmail.com>
2014-04-24 15:38:02 +0200

    test using the Gson TypeAdapter rather than the annotation based serialization

--- HttpClient-Gson/tests/src/com/levelup/http/gson/AsyncGsonParse.java
+++ HttpClient-Gson/tests/src/com/levelup/http/gson/AsyncGsonParse.java
@@ -1,6 +1,9 @@
 package com.levelup.http.gson;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,11 +11,14 @@ import junit.framework.TestCase;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 import com.levelup.http.HttpClient;
 import com.levelup.http.HttpRequestGet;
-import com.levelup.http.gson.InputStreamGsonParser;
 
 public class AsyncGsonParse extends TestCase {
 
@@ -42,5 +48,88 @@ public class AsyncGsonParse extends TestCase {
 		ArrayList<AppXoid> items = HttpClient.parseRequest(request, parser);
 		assertNotNull(items);
 	}
-	
+
+	public static class AppXoidReader {
+		@SerializedName("pk") int apkNum;
+		@SerializedName("model") String model;
+		@SerializedName("fields") AppXoidInfo data;
+	}
+
+	public static class AppXoidInfoReader {
+		public final boolean availableInFrance;
+		public final String rating;
+		public final Date dateUpdated;
+		public final String creator;
+
+		private AppXoidInfoReader(Builder builder) {
+			this.availableInFrance = builder.availableInFrance;
+			this.rating = builder.rating;
+			this.dateUpdated = builder.dateUpdated;
+			this.creator = builder.creator;
+		}
+
+		public static final class Builder {
+			public boolean availableInFrance;
+			public String rating;
+			public Date dateUpdated;
+			public String creator;
+
+			public AppXoidInfoReader build() {
+				return new AppXoidInfoReader(this);
+			}
+		}
+	}
+
+	public void testGsonDeserialize() throws Exception {
+		TypeAdapter<AppXoidInfoReader> infoAdapter = new TypeAdapter<AppXoidInfoReader>() {
+			private final SimpleDateFormat format = new SimpleDateFormat(JSON_DATE_FORMAT);
+
+			@Override
+			public void write(JsonWriter out, AppXoidInfoReader value) throws IOException {
+				throw new IllegalAccessError();
+			}
+
+			@Override
+			public AppXoidInfoReader read(JsonReader reader) throws IOException {
+				// the first token is the start object
+				JsonToken token = reader.peek();
+				AppXoidInfoReader.Builder builder = new AppXoidInfoReader.Builder();
+				if (token.equals(JsonToken.BEGIN_OBJECT)) {
+					reader.beginObject();
+					while (!reader.peek().equals(JsonToken.END_OBJECT)) {
+						if (reader.peek().equals(JsonToken.NAME)) {
+							String name = reader.nextName();
+							if ("available_fr".equals(name))
+								builder.availableInFrance = reader.nextBoolean();
+							else if ("rating".equals(name))
+								builder.rating = reader.nextString();
+							else if ("app_updated".equals(name))
+								try {
+									builder.dateUpdated = format.parse(reader.nextString());
+								} catch (ParseException e) {
+								}
+							else if ("creator".equals(name))
+								builder.creator = reader.nextString();
+							else
+								reader.skipValue();
+						}
+					}
+					reader.endObject();
+				}
+				return builder.build();
+			}
+		};
+
+		Gson gson = new GsonBuilder()
+		.registerTypeAdapter(AppXoidInfoReader.class, infoAdapter)
+		.setDateFormat(JSON_DATE_FORMAT)
+		.create();
+
+		Type listType = new TypeToken<ArrayList<AppXoidReader>>() {}.getType();
+
+		InputStreamGsonParser<ArrayList<AppXoidReader>> parser = new InputStreamGsonParser<ArrayList<AppXoidReader>>(gson, listType);
+		HttpRequestGet request = new HttpRequestGet(JSON_URL);
+		ArrayList<AppXoidReader> items = HttpClient.parseRequest(request, parser);
+		assertNotNull(items);
+	}
 }
