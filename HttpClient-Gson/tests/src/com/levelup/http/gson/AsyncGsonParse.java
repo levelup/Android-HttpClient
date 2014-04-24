6f74d51d8a7ccd99addd13490b3d6d5500b76c23
Steve Lhomme <robux4@gmail.com>
2014-04-24 15:37:02 +0200

    test code cleaning

--- HttpClient-Gson/tests/src/com/levelup/http/gson/AsyncGsonParse.java
+++ HttpClient-Gson/tests/src/com/levelup/http/gson/AsyncGsonParse.java
@@ -17,25 +17,26 @@ import com.levelup.http.gson.InputStreamGsonParser;
 public class AsyncGsonParse extends TestCase {
 
 	private static final String JSON_URL = "http://social.appxoid.com/json/get_apps_by_pages2";
-	
-	private static class AppXoidInfo {
+	private static final String JSON_DATE_FORMAT = "yyyy-mm-dd'T'HH:mm:ss";
+
+	public static class AppXoidInfo {
 		@SerializedName("available_fr") boolean availableInFrance;
-		@SerializedName("rating") String raring;
+		@SerializedName("rating") String rating;
 		@SerializedName("app_updated") Date dateUpdated;
 		@SerializedName("creator") String creator;
 	}
-	
-	private static class AppXoid {
+
+	public static class AppXoid {
 		@SerializedName("pk") int apkNum;
 		@SerializedName("model") String model;
 		@SerializedName("fields") AppXoidInfo data;
 	}
-	
+
 	public void testGsonParser() throws Exception {
-		Gson gson = new GsonBuilder().setDateFormat("yyyy-mm-dd'T'HH:mm:ss").create();
-		
+		Gson gson = new GsonBuilder().setDateFormat(JSON_DATE_FORMAT).create();
+
 		Type listType = new TypeToken<ArrayList<AppXoid>>() {}.getType();
-		
+
 		InputStreamGsonParser<ArrayList<AppXoid>> parser = new InputStreamGsonParser<ArrayList<AppXoid>>(gson, listType);
 		HttpRequestGet request = new HttpRequestGet(JSON_URL);
 		ArrayList<AppXoid> items = HttpClient.parseRequest(request, parser);
