2786dd1f904f12fc6eab2b37181b56a09d0995b0
Steve Lhomme <robux4@gmail.com>
2014-06-12 14:09:17 +0200

    keep the classes deserialized via Gson from being obfuscated

---
+++ HttpClient-Gson/src/com/levelup/http/gson/GsonReflected.java
@@ -0,0 +1,7 @@
+package com.levelup.http.gson;
+
+/**
+ * Interface to mark a class and its children as used by Gson for reflection, so that ProGuard doesn't obfuscate them
+ */
+public interface GsonReflected {
+}
