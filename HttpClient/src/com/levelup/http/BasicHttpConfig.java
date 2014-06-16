280dca709ee5e855c989233af6df49750d08fb3f
Steve Lhomme <robux4@gmail.com>
2014-06-16 10:42:37 +0200

    cleaner writing of the default HTTP read timeout values

--- HttpClient/src/com/levelup/http/BasicHttpConfig.java
+++ HttpClient/src/com/levelup/http/BasicHttpConfig.java
@@ -1,14 +1,15 @@
 package com.levelup.http;
 
 
+import android.text.format.DateUtils;
 
 /**
  * Basic HTTP configuration to access servers
  */
 public class BasicHttpConfig implements HttpConfig {
 
-	private static final int READ_TIMEOUT_IN_MS = 6000; // 6s
-	private static final int READ_TIMEOUT_LONG_POST_IN_MS = 80000; // 80s
+	private static final int READ_TIMEOUT_IN_MS = (int) (6 * DateUtils.SECOND_IN_MILLIS);
+	private static final int READ_TIMEOUT_LONG_POST_IN_MS = (int) (80 * DateUtils.SECOND_IN_MILLIS);
 
 	public static final BasicHttpConfig instance = new BasicHttpConfig();
 
