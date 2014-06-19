380fb0496b9f382f5ace66f7c46f0f3f7bc1b1f6
Steve Lhomme <robux4@gmail.com>
2014-06-19 14:53:15 +0200

    Better reporting of bad JSON data

--- HttpClient/src/com/levelup/http/InputStreamJSONArrayParser.java
+++ HttpClient/src/com/levelup/http/InputStreamJSONArrayParser.java
@@ -19,9 +19,9 @@ public class InputStreamJSONArrayParser implements InputStreamParser<JSONArray> 
 		try {
 			return new JSONArray(srcData);
 		} catch (JSONException e) {
-			throw new ParserException("Bad JSON data "+srcData+' '+e.getMessage(), request.newException().setCause(e).setErrorCode(HttpException.ERROR_JSON).build());
+			throw new ParserException("Bad JSON data", request.newException().setErrorMessage("Bad JSON data "+srcData+' '+e.getMessage()).setCause(e).setErrorCode(HttpException.ERROR_JSON).build());
 		} catch (NullPointerException e) {
-			throw new ParserException("Invalid JSON data "+srcData, e);
+			throw new ParserException("Invalid JSON data", request.newException().setErrorMessage("Invalid JSON data "+srcData).setCause(e).build());
 		}
 	}
 }
--- HttpClient/src/com/levelup/http/InputStreamJSONObjectParser.java
+++ HttpClient/src/com/levelup/http/InputStreamJSONObjectParser.java
@@ -19,9 +19,9 @@ public class InputStreamJSONObjectParser implements InputStreamParser<JSONObject
 		try {
 			return new JSONObject(srcData);
 		} catch (JSONException e) {
-			throw new ParserException("Bad JSON data "+srcData+' '+e.getMessage(), request.newException().setCause(e).setErrorCode(HttpException.ERROR_JSON).build());
+			throw new ParserException("Bad JSON data", request.newException().setErrorMessage("Bad JSON data "+srcData+' '+e.getMessage()).setCause(e).setErrorCode(HttpException.ERROR_JSON).build());
 		} catch (NullPointerException e) {
-			throw new ParserException("Invalid JSON data "+srcData, request.newException().setCause(e).build());
+			throw new ParserException("Invalid JSON data", request.newException().setErrorMessage("Invalid JSON data "+srcData).setCause(e).build());
 		}
 	}
 }
