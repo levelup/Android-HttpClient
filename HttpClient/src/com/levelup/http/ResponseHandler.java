58e8d189d65efda76c74baba8b5d5682321a7f9c
Steve Lhomme <robux4@gmail.com>
2014-08-26 14:19:20 +0200

    clean ResponseHandler after renaming

--- HttpClient/src/com/levelup/http/ResponseHandler.java
+++ HttpClient/src/com/levelup/http/ResponseHandler.java
@@ -5,18 +5,18 @@ import com.levelup.http.parser.XferTransform;
 /**
  * Created by robUx4 on 20/08/2014.
  */
-public class HttpResponseHandler<OUTPUT> {
+public class ResponseHandler<OUTPUT> {
 
 	public final XferTransform<HttpResponse, OUTPUT> contentParser;
 	public final ErrorHandler errorHandler;
 
-	public HttpResponseHandler(XferTransform<HttpResponse, OUTPUT> contentParser, ErrorHandler errorHandler) {
+	public ResponseHandler(XferTransform<HttpResponse, OUTPUT> contentParser, ErrorHandler errorHandler) {
 		if (null == contentParser) throw new NullPointerException("we need a parser for the content");
 		this.contentParser = contentParser;
 		this.errorHandler = errorHandler;
 	}
 
-	public HttpResponseHandler(XferTransform<HttpResponse, OUTPUT> contentParser) {
+	public ResponseHandler(XferTransform<HttpResponse, OUTPUT> contentParser) {
 		this(contentParser, BaseErrorHandler.INSTANCE);
 	}
 }
