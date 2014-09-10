4ef287405403e72ae9b501c95400ca998be2007e
Steve Lhomme <robux4@gmail.com>
2014-09-10 10:20:03 +0200

    ErrorHandlerViaXferTransform is the preffered ErrorHandler class

--- HttpClient/src/com/levelup/http/ResponseHandler.java
+++ HttpClient/src/com/levelup/http/ResponseHandler.java
@@ -12,6 +12,12 @@ public class ResponseHandler<OUTPUT> {
 	public final XferTransform<HttpResponse, OUTPUT> contentParser;
 	public final ErrorHandler errorHandler;
 
+	/**
+	 * Main Constructor
+	 * @param contentParser
+	 * @param errorHandler
+	 * @see com.levelup.http.parser.ErrorHandlerViaXferTransform ErrorHandlerViaXferTransform for a common errorHandler
+	 */
 	public ResponseHandler(XferTransform<HttpResponse, OUTPUT> contentParser, ErrorHandler errorHandler) {
 		if (null == contentParser) throw new NullPointerException("we need a parser for the content");
 		if (null == errorHandler) throw new NullPointerException("we need an error handler, consider BaseErrorHandler");
